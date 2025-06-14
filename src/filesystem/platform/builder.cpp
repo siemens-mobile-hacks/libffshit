60abffb336d5f7534644136e1c61bc97d19157a4
Roman Serov <roman@serov.co>
2025-06-14 14:18:05 +0300

    default

--- src/filesystem/platform/builder.cpp
+++ src/filesystem/platform/builder.cpp
@@ -8,6 +8,7 @@ Filesystem::Base::Ptr build(Platform platform, Blocks &blocks) {
         case FULLFLASH::Platform::X65: return FULLFLASH::Filesystem::SGOLD::build(blocks);
         case FULLFLASH::Platform::X75: return FULLFLASH::Filesystem::NewSGOLD::build(blocks);
         case FULLFLASH::Platform::X85: return FULLFLASH::Filesystem::NewSGOLD_X85::build(blocks);
+        default:
         case FULLFLASH::Platform::UNK: {
             throw FULLFLASH::Exception("Unknown platform");
         }

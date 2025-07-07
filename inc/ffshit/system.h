2de0aafa8d2167a4acffc1fd12a5e227ad24aff4
Roman Serov <roman@serov.co>
2025-07-07 16:45:10 +0300

    Время/дата для корня. Возврат кода ошибки для create/remove dir

--- inc/ffshit/system.h
+++ inc/ffshit/system.h
@@ -3,13 +3,14 @@
 
 #include <string>
 #include <filesystem>
+#include <system_error>
 
 namespace System {
 
 bool is_file_exists(const std::filesystem::path path);
 bool is_directory_exists(const std::filesystem::path path);
-bool remove_directory(const std::filesystem::path path);
-bool create_directory(const std::filesystem::path path, std::filesystem::perms perms);
+bool remove_directory(const std::filesystem::path path, std::error_code &error_code);
+bool create_directory(const std::filesystem::path path, std::filesystem::perms perms, std::error_code &error_code);
 
 void to_lower(std::string &str);
 void to_upper(std::string &str);
